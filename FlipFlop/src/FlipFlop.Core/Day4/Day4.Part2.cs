6cfa38072f762135ff58f5ecc23b761089fc10f9
borgoo <borgo-ale@libero.it>
2025-12-18 15:01:14 +0100

    refactor(day4): simplify distance calculations in Part 2 and optimize Part 3 logic

--- FlipFlop/src/FlipFlop.Core/Day4/Day4.Part2.cs
+++ FlipFlop/src/FlipFlop.Core/Day4/Day4.Part2.cs
@@ -2,9 +2,6 @@ namespace FlipFlop.Core.Day4;
 
 internal static partial class Day4 {
 
-    /// <summary>
-    /// This could have be done with Chebyshev distance instead of Euclidean distance.
-    /// </summary>
     internal static class Part2 {
 
         internal static long Solve(string rawText) {
@@ -12,23 +9,12 @@ internal static partial class Day4 {
             int[] prev = [0,0];
             int steps = 0;
 
-            double sqrt2 = Math.Sqrt(2);
-
             foreach(string line in rawText.Split(Environment.NewLine)) {
 
                 int[] curr = [.. line.Split(',').Select(int.Parse)];
                 int deltaX = Math.Abs(curr[0] - prev[0]);
                 int deltaY = Math.Abs(curr[1] - prev[1]);
-                double hypotenuse = Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
-                int max = Math.Max(deltaX, deltaY);
-                if(hypotenuse >= max) {
-                    steps +=  max;
-                    prev = curr;
-                    continue;
-                }
-                int approxHypotenuse = (int)Math.Ceiling(hypotenuse / sqrt2);
-                steps += approxHypotenuse;
-                
+                steps += Math.Max(deltaX, deltaY);
                 prev = curr;
             }
             return steps;
--- FlipFlop/src/FlipFlop.Core/Day4/Day4.Part3.cs
+++ FlipFlop/src/FlipFlop.Core/Day4/Day4.Part3.cs
@@ -6,28 +6,14 @@ internal static partial class Day4 {
 
     internal static class Part3 {
 
-        private static int ManhattanDistance(int[] position) {
-            return position[0] + position[1];
-        }
+        private static int ManhattanDistance(int[] position) => position[0] + position[1];
 
         internal static long Solve(string rawText) {
 
             List<int[]> trashPositions = [.. rawText.Split(Environment.NewLine).Select(line => line.Split(',').Select(int.Parse).ToArray())];
             trashPositions.Sort((a, b) =>  ManhattanDistance(a).CompareTo(ManhattanDistance(b)));
-            
-            int[] prev = [0,0];
-            int steps = 0;
-             foreach(int[] curr in trashPositions) {
-
-                int deltaX = Math.Abs(curr[0] - prev[0]);
-                int deltaY = Math.Abs(curr[1] - prev[1]);
-                int chebyshevDistance = (deltaX + deltaY + Math.Abs(deltaX - deltaY)) / 2;
-                steps += chebyshevDistance;
-                
-                prev = curr;
-            }
-            return steps;
-
+            string sortedRawText =string.Join(Environment.NewLine, trashPositions.Select(p => p[0]+","+p[1]));
+            return Part2.Solve(sortedRawText);
 
         }
 
